d51af9d5d132288d6517115c7e4b1f5324dad91a
Linus Nagel <linusnag@gmail.com>
2024-11-23 14:08:58 +0100

    unit test rook movegen

--- src/engine/move_iter/rook/mod.rs
+++ src/engine/move_iter/rook/mod.rs
@@ -1,5 +1,8 @@
 use crate::{engine::{bitboard::Bitboard, coordinates::{CompassRose, File, Rank, Square}}, misc::ConstFrom};
 
+#[cfg(test)]
+mod tests;
+
 /// Computes the attacks of the rook on the square `sq` with the given `occupancy`.
 const fn compute_attacks(sq: Square, occupancy: Bitboard) -> Bitboard {
     let file = File::from_c(sq);
@@ -9,12 +12,12 @@ const fn compute_attacks(sq: Square, occupancy: Bitboard) -> Bitboard {
     let nort_bb = Bitboard::split_north(sq);
     let sout_bb = Bitboard::split_south(sq);
     let mut result = Bitboard::empty();
-    
+
     // south
     let ray = file_bb.v & sout_bb.v;
     let occupands = Bitboard { v: occupancy.v & ray };
     let nearest = occupands.msb();
-    // todo: this is an inlined 'map_or'. When nightly rust provides 
+    // todo: this is an inlined 'map_or'. When nightly rust provides
     // it, this should be replaced again. The others are the same
     let range = {
         let default = Bitboard::full();
@@ -26,29 +29,29 @@ const fn compute_attacks(sq: Square, occupancy: Bitboard) -> Bitboard {
     };
     let moves = range.shift_c::<{CompassRose::SOUT.v()}>().v & ray;
     result.v |= moves;
-    
+
     // north
     let ray = file_bb.v & nort_bb.v;
     let occupands = Bitboard { v: occupancy.v & ray };
     let nearest = occupands.lsb();
     let range = match nearest {
         Some(t) => Bitboard::split_south(t),
-        None => Bitboard::empty(),
+        None => Bitboard::full(),
     };
     let moves = range.shift_c::<{CompassRose::NORT.v()}>().v & ray;
     result.v |= moves;
-    
+
     // west
     let ray = rank_bb.v & sout_bb.v;
     let occupands = Bitboard { v: occupancy.v & ray };
     let nearest = occupands.msb();
     let range = match nearest {
         Some(t) => Bitboard::split_north(t),
-        None => Bitboard::empty(),
+        None => Bitboard::full(),
     };
     let moves = range.shift_c::<{CompassRose::WEST.v()}>().v & ray;
     result.v |= moves;
-    
+
     // east
     let ray = rank_bb.v & nort_bb.v;
     let occupands = Bitboard { v: occupancy.v & ray };
@@ -59,6 +62,6 @@ const fn compute_attacks(sq: Square, occupancy: Bitboard) -> Bitboard {
     };
     let moves = range.shift_c::<{CompassRose::EAST.v()}>().v & ray;
     result.v |= moves;
-    
+
     result
 }
