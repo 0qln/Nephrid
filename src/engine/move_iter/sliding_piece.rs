9def5d9354cdad0779c45fa130cb44504c80088e
Linus Nagel <linusnag@gmail.com>
2024-11-30 16:11:47 +0100

    refactoring

--- src/engine/move_iter/sliding_piece.rs
+++ src/engine/move_iter/sliding_piece.rs
@@ -1,5 +1,5 @@
 use crate::{engine::{
-    bitboard::Bitboard, color::{self, Color}, coordinates::Square, r#move::{Move, MoveFlag}, piece::{PieceType, SlidingPieceType}, position::Position
+    bitboard::Bitboard, color::{self, Color}, coordinates::Square, r#move::{Move, MoveFlag}, piece::{PieceType, SlidingPieceType}, position::{CheckState, Position}
 }, misc::ConstFrom};
 
 use super::{gen_captures, gen_quiets};
@@ -19,34 +19,32 @@ pub fn gen_legal_check_none(
     piece_type: SlidingPieceType,
     compute_attacks: fn(Square, Bitboard) -> Bitboard,
 ) -> impl Iterator<Item = Move> {
+    assert_eq!(pos.get_check_state(), CheckState::None);
     let color = pos.get_turn();
     let allies = pos.get_color_bb(color);
     let enemies = pos.get_color_bb(!color);
+    let occupancy = allies | enemies;
     let blockers = pos.get_blockers();
     let king_bb = pos.get_bitboard(PieceType::KING, color);
+    // Safety: king the board has no king, but gen_legal is used,
+    // the context is broken anyway. 
+    let king = unsafe { king_bb.lsb().unwrap_unchecked() };
     pos.get_bitboard(piece_type.into(), color)
         .flat_map(move |piece| {
-            let attacks = compute_attacks(piece, allies | enemies);
-            let capture_targets = attacks & enemies;
-            let quiet_targets = attacks & !allies & !enemies;
             let piece_bb = Bitboard::from_c(piece);
             let is_blocker = !(blockers & piece_bb).is_empty(); 
-            // Restrict the movement of pinned pieces. 
-            capture_targets.filter_map(move |target| {
-                let is_illegal = is_blocker && (Bitboard::ray(piece, target) & king_bb).is_empty();
-                match is_illegal {
-                    false => Some(Move::new(piece, target, MoveFlag::CAPTURE)),
-                    true => None
-                }
-            }).chain(
-                quiet_targets.filter_map(move |target| {
-                    let is_illegal = is_blocker && (Bitboard::ray(piece, target) & king_bb).is_empty();
-                    match is_illegal {
-                        false => Some(Move::new(piece, target, MoveFlag::QUIET)),
-                        true => None
-                    }
-                })
-            )
+            let pin_mask = if is_blocker { Bitboard::ray(piece, king) } else { Bitboard::full() };
+            let attacks = compute_attacks(piece, occupancy);
+            let legal_attacks = attacks & pin_mask;
+            let captures = {
+                let targets = legal_attacks & enemies;
+                targets.map(move |target| Move::new(piece, target, MoveFlag::CAPTURE))               
+            };
+            let quiets = {
+                let targets = legal_attacks & !allies & !enemies;
+                targets.map(move |target| Move::new(piece, target, MoveFlag::QUIET))
+            };
+            captures.chain(quiets)
         })
 }
 
