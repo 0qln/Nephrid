27b35a79122943da35ba4f3c10811b0da17d0dfc
Linus Nagel <linusnag@gmail.com>
2025-01-18 16:02:37 +0100

    bug fix

--- src/engine/move_iter/king.rs
+++ src/engine/move_iter/king.rs
@@ -203,31 +203,31 @@ pub fn compute_attacks(sq: Square) -> Bitboard {
     let file = File::from_c(sq);
     let rank = Rank::from_c(sq);
 
-    let mut result = Bitboard::empty();
-
+    let mut files = Bitboard::from_c(file);
     if file > File::A {
         // Safety: file is in range 1.., so file - 1 is still a valid file.
         let west = unsafe { File::from_v(file.v() - 1) };
-        result |= Bitboard::from_c(west);
+        files |= Bitboard::from_c(west);
     }
 
     if file < File::H {
         // Safety: file is in range 0..7, so file + 1 is still a valid file.
         let east = unsafe { File::from_v(file.v() + 1) };
-        result |= Bitboard::from_c(east);
+        files |= Bitboard::from_c(east);
     }
 
+    let mut ranks = Bitboard::from_c(rank);
     if rank > Rank::_1 {
         // Safety: rank is in range 1.., so rank - 1 is still a valid rank.
         let south = unsafe { Rank::from_v(rank.v() - 1) };
-        result |= Bitboard::from_c(south);
+        ranks |= Bitboard::from_c(south);
     }
 
     if rank < Rank::_8 {
         // Safety: rank is in range 0..7, so rank + 1 is still a valid rank.
         let north = unsafe { Rank::from_v(rank.v() + 1) };
-        result |= Bitboard::from_c(north);
+        ranks |= Bitboard::from_c(north);
     }
 
-    result
+    files.and_c(ranks) ^ Bitboard::from_c(sq)
 }
