a3a6c77cfcd458a1e408e9a870b6f2a41a247849
Linus Nagel <linusnag@gmail.com>
2025-01-18 18:07:57 +0100

    Update jumping_piece.rs

--- src/engine/move_iter/jumping_piece.rs
+++ src/engine/move_iter/jumping_piece.rs
@@ -39,6 +39,37 @@ pub fn gen_legals_check_none(
         })
 }
 
+pub fn gen_legals_check_single(
+    pos: &Position,
+    piece_type: JumpingPieceType,
+    compute_attacks: fn(Square) -> Bitboard,
+) -> impl Iterator<Item = Move> + '_ {
+    assert_eq!(pos.get_check_state(), CheckState::Single);
+    let color = pos.get_turn();
+    pos.get_bitboard(piece_type.into(), color)
+        .flat_map(move |piece| {
+            let enemies = pos.get_color_bb(!color);
+            let allies = pos.get_color_bb(color);
+            let blockers = pos.get_blockers();
+            let king_bb = pos.get_bitboard(PieceType::KING, color);
+            // Safety: king the board has no king, but gen_legal is used,
+            // the context is broken anyway.
+            let king = unsafe { king_bb.lsb().unwrap_unchecked() };
+            // Safety: there is a single checker.
+            let checker = unsafe { pos.get_checkers().lsb().unwrap_unchecked() };
+            let resolves = Bitboard::between(king, checker);
+            let piece_bb = Bitboard::from_c(piece);
+            let is_not_blocker = (blockers & piece_bb).is_empty();
+            let legal_attacks = is_not_blocker
+                .then(|| compute_attacks(piece))
+                .unwrap_or_default();
+            let legal_resolves = resolves & legal_attacks;
+            let captures = gen_captures(legal_resolves, enemies, piece);
+            let quiets = gen_quiets(legal_resolves, enemies, allies, piece);
+            captures.chain(quiets)
+        })
+}
+
 pub fn gen_legal_captures_check_none(
     pos: &Position,
     piece_type: JumpingPieceType,
