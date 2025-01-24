e8d4b8c0664333e8612a35fcb39da181e433fafb
Linus Nagel <linusnag@gmail.com>
2025-01-24 21:25:16 +0100

    Update queen.rs

--- src/engine/move_iter/queen.rs
+++ src/engine/move_iter/queen.rs
@@ -1,6 +1,22 @@
 use crate::engine::{bitboard::Bitboard, coordinates::Square, r#move::Move, piece::SlidingPieceType, position::Position};
 
-use super::{bishop, rook, sliding_piece};
+use super::{bishop::{self, Bishop}, rook::{self, Rook}, sliding_piece::{self, Attacks}};
+
+pub struct Queen;
+
+impl Attacks for Queen {
+    fn compute_attacks_0_occ(sq: Square) -> Bitboard {
+        Rook::compute_attacks_0_occ(sq) | Bishop::compute_attacks_0_occ(sq)
+    }
+
+    fn compute_attacks(sq: Square, occupancy: Bitboard) -> Bitboard {
+        Rook::compute_attacks(sq, occupancy) | Bishop::compute_attacks(sq, occupancy)
+    }
+
+    fn lookup_attacks(sq: Square, occupancy: Bitboard) -> Bitboard {
+        Rook::lookup_attacks(sq, occupancy) | Bishop::lookup_attacks(sq, occupancy)
+    }
+}
 
 #[inline]
 pub fn gen_legals_check_none(pos: &Position) -> impl Iterator<Item = Move> + '_ {
