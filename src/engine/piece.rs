8ca1d6f7b088b16da0a7b65a24177f8c052c4d82
Linus Nagel <linusnag@gmail.com>
2025-01-25 18:08:03 +0100

    bug fix

--- src/engine/piece.rs
+++ src/engine/piece.rs
@@ -245,6 +245,6 @@ impl fmt::Debug for Piece {
 
 impl fmt::Display for Piece {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        writeln!(f, "{}", Into::<char>::into(*self))
+        write!(f, "{}", Into::<char>::into(*self))
     }
 }
