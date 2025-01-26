5199ba2bfb7e1ad79f862e77d802a0408828a0c5
Linus Nagel <linusnag@gmail.com>
2025-01-26 20:07:51 +0100

    clean up rook

--- src/engine/move_iter/rook/mod.rs
+++ src/engine/move_iter/rook/mod.rs
@@ -1,6 +1,6 @@
 use crate::{
     engine::{
-        bitboard::Bitboard, coordinates::{CompassRose, File, Rank, Square}, r#move::Move, piece::{IPieceType, PieceType, SlidingPieceType}, position::Position
+        bitboard::Bitboard, coordinates::{CompassRose, File, Rank, Square}, piece::{IPieceType, PieceType}
     },
     misc::ConstFrom,
 };
@@ -71,7 +71,7 @@ pub const fn compute_attacks_0_occ(sq: Square) -> Bitboard {
 }
 
 /// Computes the attacks of the rook on the square `sq` with the given `occupancy`.
-const fn compute_attacks(sq: Square, occupancy: Bitboard) -> Bitboard {
+fn compute_attacks(sq: Square, occupancy: Bitboard) -> Bitboard {
     let file = File::from_c(sq);
     let rank = Rank::from_c(sq);
     let file_bb = Bitboard::from_c(file);
@@ -81,62 +81,36 @@ const fn compute_attacks(sq: Square, occupancy: Bitboard) -> Bitboard {
     let mut result = Bitboard::empty();
 
     // south
-    let ray = file_bb.v & sout_bb.v;
-    let occupands = Bitboard {
-        v: occupancy.v & ray,
-    };
+    let ray = file_bb & sout_bb;
+    let occupands = occupancy & ray;
     let nearest = occupands.msb();
-    // todo: this is an inlined 'map_or'. When nightly rust provides
-    // it, this should be replaced again. The others are the same
-    let range = {
-        let default = Bitboard::full();
-        let f = Bitboard::split_north;
-        match nearest {
-            Some(t) => f(t),
-            None => default,
-        }
-    };
-    let moves = range.shift_c::<{ CompassRose::SOUT.v() }>().v & ray;
-    result.v |= moves;
+    let range = nearest.map_or(Bitboard::full(), |t| Bitboard::split_north(t));
+    let moves = range.shift_c::<{ CompassRose::SOUT.v() }>() & ray;
+    result |= moves;
 
     // north
-    let ray = file_bb.v & nort_bb.v;
-    let occupands = Bitboard {
-        v: occupancy.v & ray,
-    };
+    let ray = file_bb & nort_bb;
+    let occupands = occupancy & ray;
     let nearest = occupands.lsb();
-    let range = match nearest {
-        Some(t) => Bitboard::split_south(t),
-        None => Bitboard::full(),
-    };
-    let moves = range.shift_c::<{ CompassRose::NORT.v() }>().v & ray;
-    result.v |= moves;
+    let range = nearest.map_or(Bitboard::full(), |t| Bitboard::split_south(t));
+    let moves = range.shift_c::<{ CompassRose::NORT.v() }>() & ray;
+    result |= moves;
 
     // west
-    let ray = rank_bb.v & sout_bb.v;
-    let occupands = Bitboard {
-        v: occupancy.v & ray,
-    };
+    let ray = rank_bb & sout_bb;
+    let occupands = occupancy & ray;
     let nearest = occupands.msb();
-    let range = match nearest {
-        Some(t) => Bitboard::split_north(t),
-        None => Bitboard::full(),
-    };
-    let moves = range.shift_c::<{ CompassRose::WEST.v() }>().v & ray;
-    result.v |= moves;
+    let range = nearest.map_or(Bitboard::full(), |t| Bitboard::split_north(t));
+    let moves = range.shift_c::<{ CompassRose::WEST.v() }>() & ray;
+    result |= moves;
 
     // east
-    let ray = rank_bb.v & nort_bb.v;
-    let occupands = Bitboard {
-        v: occupancy.v & ray,
-    };
+    let ray = rank_bb & nort_bb;
+    let occupands = occupancy & ray;
     let nearest = occupands.lsb();
-    let range = match nearest {
-        Some(t) => Bitboard::split_south(t),
-        None => Bitboard::full(),
-    };
-    let moves = range.shift_c::<{ CompassRose::EAST.v() }>().v & ray;
-    result.v |= moves;
+    let range = nearest.map_or(Bitboard::full(), |t| Bitboard::split_south(t));
+    let moves = range.shift_c::<{ CompassRose::EAST.v() }>() & ray;
+    result |= moves;
 
     result
 }
