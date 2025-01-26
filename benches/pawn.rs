5086a3dc2f033e05c13016eb44a999c8ac5f19a0
Linus Nagel <linusnag@gmail.com>
2025-01-26 21:17:10 +0100

    add bench pawn::legals_check_none

--- benches/pawn.rs
+++ benches/pawn.rs
@@ -1,5 +1,17 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use nephrid::engine::{color::Color, coordinates::Square, move_iter::pawn::lookup_attacks};
+use std::ops::ControlFlow;
+
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
+use nephrid::engine::{
+    color::Color,
+    coordinates::Square,
+    fen::Fen,
+    move_iter::{
+        pawn::{self, lookup_attacks},
+        sliding_piece::magics,
+    },
+    position::Position,
+    r#move::Move,
+};
 
 pub fn pawn_attacks(c: &mut Criterion) {
     let pawn = Square::E4;
@@ -12,8 +24,33 @@ pub fn pawn_attacks(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(
-    benches,
-    pawn_attacks,
-);
+pub fn move_iter_check_none(c: &mut Criterion) {
+    magics::init(0xdead_beef);
+
+    let inputs = [
+        "K4n1n/6P1/k7/1p1pP3/2P5/8/5P2/8 w - d6 0 1",
+    ];
+
+    for &input in &mut inputs.iter() {
+        let mut fen = Fen::new(input);
+        let pos = Position::try_from(&mut fen).unwrap();
+        c.bench_with_input(
+            BenchmarkId::new("pawn::move_iter::check_none", input),
+            &pos,
+            |b, pos| {
+                b.iter(|| {
+                    pawn::fold_legals_check_none(
+                        black_box(&pos),
+                        black_box(0),
+                        black_box(|acc, m: Move| {
+                            ControlFlow::Continue::<(), _>(acc + m.get_to().v())
+                        }),
+                    )
+                })
+            },
+        );
+    }
+}
+
+criterion_group!(benches, pawn_attacks, move_iter_check_none);
 criterion_main!(benches);
