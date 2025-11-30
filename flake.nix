810a1f7cf775140d3af8165ae171b8145b50bd56
0qln <0qln@proton.me>
2025-11-30 18:05:30 +0100

    add bacon to dev tools

--- flake.nix
+++ flake.nix
@@ -51,6 +51,7 @@
             packages =
               (with pkgs; [
                 cutechess
+                bacon
               ])
               ++ (with pkgs-cuda; [
                 # reference: https://discourse.nixos.org/t/cuda-12-8-support-in-nixpkgs/60645/39
