db7f056669462a3903c1a09cd89b2f1c3a8cf175
0qln <0qln@proton.me>
2026-02-13 18:20:53 +0100

    fix link

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -21,7 +21,7 @@
 #![feature(new_range_api)]
 #![feature(range_into_bounds)]
 #![feature(control_flow_into_value)]
-// todo when feature (github.com/rust-lang/rust/issues/67792) is complete:
+// todo when feature (https://github.com/rust-lang/rust/issues/67792) is complete:
 // - remove the ConstFrom trait and use the core::From trait instead.
 
 #[macro_use]
