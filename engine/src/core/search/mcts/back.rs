863ee75362009eb1d08038fa9c2ef50f206f3d37
oq_ <129159411+0qln@users.noreply.github.com>
2026-03-23 18:03:41 +0100

    add value/visit updates for leaf nodes (#60)
    ccbench:
    ```
    Score of nephrid-feat vs nephrid-base: 222 - 108 - 31  [0.658] 361
    ...      nephrid-feat playing White: 112 - 54 - 15  [0.660] 181
    ...      nephrid-feat playing Black: 110 - 54 - 16  [0.656] 180
    ...      White vs Black: 166 - 164 - 31  [0.503] 361
    Elo difference: 113.6 +/- 36.0, LOS: 100.0 %, DrawRatio: 8.6 %
    SPRT: llr 0.72 (24.4%), lbound -2.94, ubound 2.94
    
    Player: nephrid-feat
       "Draw by 3-fold repetition": 1
       "Draw by fifty moves rule": 23
       "Draw by insufficient mating material": 7
       "Loss: Black mates": 54
       "Loss: White mates": 54
       "No result": 6
       "Win: Black mates": 110
       "Win: White mates": 112
    Player: nephrid-base
       "Draw by 3-fold repetition": 1
       "Draw by fifty moves rule": 23
       "Draw by insufficient mating material": 7
       "Loss: Black mates": 110
       "Loss: White mates": 112
       "No result": 6
       "Win: Black mates": 54
       "Win: White mates": 54
    Finished match
    ```
    
    * add value/visit updates for leaf nodes
    
    * Bump up crate version to 26.3.23+1442c14

--- engine/src/core/search/mcts/back.rs
+++ engine/src/core/search/mcts/back.rs
@@ -4,7 +4,7 @@ use crate::core::search::mcts::{
     eval::{self, Evaluation, GameResult},
     node::{
         Tree,
-        node_state::{self, Branching, Evaluated, Terminal},
+        node_state::{self, NodeSwitch},
         proven,
     },
     search::{SelNode, Selection},
@@ -37,20 +37,19 @@ impl Backpropagater for DefaultBackuper {
             let node = &leaf.node;
             let value = eval.to_value(!leaf.turn);
 
-            if let Some(unevaluated) = node.clone().try_into::<Branching>() {
-                let evaluated = if let Evaluation::Guess(guess) = eval {
-                    tree.set_policy(unevaluated, &guess.policy)
+            match node.clone().into_ct() {
+                NodeSwitch::Branching(node) => {
+                    let evaluated = if let Evaluation::Guess(guess) = eval {
+                        tree.set_policy(node, &guess.policy)
+                    }
+                    else {
+                        tree.skip_policy(node)
+                    };
+                    tree.update_node(evaluated, value);
                 }
-                else {
-                    tree.skip_policy(unevaluated)
-                };
-                tree.update_node(evaluated, value);
-            }
-            else if let Some(evaluated) = node.clone().try_into::<Evaluated>() {
-                tree.update_node(evaluated, value);
-            }
-            else if let Some(terminal) = node.clone().try_into::<Terminal>() {
-                tree.update_node(terminal, value);
+                NodeSwitch::Evaluated(node) => tree.update_node(node, value),
+                NodeSwitch::Terminal(node) => tree.update_node(node, value),
+                NodeSwitch::Leaf(node) => tree.update_node(node, value),
             }
         }
 
@@ -83,31 +82,35 @@ impl Backpropagater for MctsSolver {
             let turn = leaf.turn;
             let value = eval.to_value(!turn);
 
-            if let Some(unevaluated) = node.clone().try_into::<Branching>() {
-                let evaluated = if let Evaluation::Guess(guess) = eval {
-                    tree.set_policy(unevaluated, &guess.policy)
+            match node.clone().into_ct() {
+                NodeSwitch::Branching(node) => {
+                    let evaluated = if let Evaluation::Guess(guess) = eval {
+                        tree.set_policy(node, &guess.policy)
+                    }
+                    else {
+                        tree.skip_policy(node)
+                    };
+                    tree.update_node(evaluated, value);
                 }
-                else {
-                    tree.skip_policy(unevaluated)
-                };
-                tree.update_node(evaluated, value);
-            }
-            else if let Some(evaluated) = node.clone().try_into::<Evaluated>() {
-                tree.update_node(evaluated, value);
-            }
-            else if let Some(terminal) = node.clone().try_into::<Terminal>() {
-                match eval {
+                NodeSwitch::Evaluated(node) => tree.update_node(node, value),
+                NodeSwitch::Terminal(node) => match eval {
                     Evaluation::Terminal(game_result) => match *game_result {
                         GameResult::Win { relative_to } if relative_to == turn => {
-                            tree.set_proven(terminal, proven::LOSS)
+                            tree.set_proven(node, proven::LOSS)
                         }
                         GameResult::Win { relative_to } if relative_to != turn => {
-                            tree.set_proven(terminal, proven::WIN)
+                            tree.set_proven(node, proven::WIN)
                         }
-                        _ => tree.update_node(terminal, value),
+                        _ => tree.update_node(node, value),
                     },
-                    _ => tree.update_node(terminal, value),
-                };
+                    _ => tree.update_node(node, value),
+                },
+
+                // There could be a leaf node here (e.g. in the case of a 2fold-shortcut.) Make sure
+                // that we also update leaf node values. Otherwise the exploration score of such a
+                // node will be overinflated and the searcher will get stuck picking
+                // this leaf node.
+                NodeSwitch::Leaf(node) => tree.update_node(node, value),
             }
         }
 
