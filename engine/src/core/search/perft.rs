2861337bd368c3fab333b89e33d2a88b5d4f5d82
0qln <0qln@proton.me>
2026-04-12 12:43:54 +0200

    remove dead code

--- engine/src/core/search/perft.rs
+++ engine/src/core/search/perft.rs
@@ -3,13 +3,13 @@ use crate::{
         Depth, Limit, Move,
         color::{Perspective, colors, perspectives},
         r#move::MoveList,
-        move_iter::{fold_legal_moves, fold_legals},
+        move_iter::fold_legals,
         position::Position,
     },
     misc::DebugMode,
     uci::sync::{self, CancellationToken},
 };
-use std::{cell::UnsafeCell, ops::ControlFlow};
+use std::ops::ControlFlow;
 
 #[cfg(test)]
 pub mod test;
@@ -42,51 +42,6 @@ pub fn perft<const Q: bool>(
             });
         },
     )
-}
-
-#[allow(unused)]
-fn perft_inner_iter(
-    pos: &mut UnsafeCell<Position>,
-    depth: Depth,
-    limit: &Limit,
-    cancellation_token: &CancellationToken,
-    debug: &DebugMode,
-    f: fn(Move, u64, Depth, bool) -> (),
-) -> u64 {
-    if cancellation_token.is_cancelled() {
-        return 0;
-    }
-
-    if depth <= Depth::ROOT {
-        return 1;
-    }
-
-    // this is actually not sound :-)
-    // you'd have to create and destroy the &*pos.get() pointer each time when you
-    // want to read inside fold_legal_moves instead of holding it across the
-    // pos.get_mut() pointer aliases.
-    // see: https://doc.rust-lang.org/nightly/core/cell/struct.UnsafeCell.html#aliasing-rules
-    //
-    // considering the fact that this is barely any faster than just generating the
-    // moves up front and the iterating over them, this should not be used.
-    unsafe {
-        fold_legal_moves::<_, _, _>(&*pos.get(), 0, |acc, m| {
-            pos.get_mut().make_move(m);
-            let c = perft_inner_iter(
-                pos,
-                depth - 1,
-                limit,
-                cancellation_token,
-                debug,
-                if debug.get() { f } else { |_, _, _, _| {} },
-            );
-            f(m, c, limit.depth - depth, debug.get());
-            pos.get_mut().unmake_move(m);
-            ControlFlow::Continue::<(), _>(acc + c)
-        })
-        .continue_value()
-        .unwrap()
-    }
 }
 
 pub fn perft_inner_collect(
