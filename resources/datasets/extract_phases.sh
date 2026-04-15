1a1ed82302e8cb125bc0f951ee472395353831db
0qln <0qln@proton.me>
2026-04-16 00:21:25 +0200

    add phase 2

---
+++ resources/datasets/extract_phases.sh
@@ -0,0 +1,3 @@
+python resources/datasets/extract_phase.py --phase 0 --themes "mateIn1"
+python resources/datasets/extract_phase.py --phase 1 --themes "mateIn1,mateIn2"
+python resources/datasets/extract_phase.py --phase 2 --themes "mateIn1,mateIn2,mateIn3"
