9cc20176f270d29d154ba5311721630a8df9fb19
0qln <0qln@proton.me>
2026-05-01 19:03:22 +0200

    more phases

--- resources/datasets/extract_phases.sh
+++ resources/datasets/extract_phases.sh
@@ -1,3 +1,11 @@
+#!/usr/bin/env bash
+
+# learn endgames
 python resources/datasets/extract_phase.py --phase 0 --themes "mateIn1"
 python resources/datasets/extract_phase.py --phase 1 --themes "mateIn1,mateIn2"
 python resources/datasets/extract_phase.py --phase 2 --themes "mateIn1,mateIn2,mateIn3"
+python resources/datasets/extract_phase.py --phase 3 --themes "mateIn2,mateIn3"
+python resources/datasets/extract_phase.py --phase 4 --themes "mateIn3,mateIn4"
+python resources/datasets/extract_phase.py --phase 5 --themes "mateIn4,mateIn5"
+
+# learn middlegames
