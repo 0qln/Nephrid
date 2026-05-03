6dee39f7bcc304e2fd58b2698b2013d9abbbcef7
0qln <0qln@proton.me>
2026-05-03 16:26:46 +0200

    test for shortest mate finding

--- engine/src/core/search/mcts/test/mod.rs
+++ engine/src/core/search/mcts/test/mod.rs
@@ -1,6 +1,12 @@
-use crate::{core::search::mcts::{
-    eval::Policy, nn::RawLogits, node::{NodeId, Tree, node_state::HasBranches}, search::{BatchItem, Selection}
-}, misc::List};
+use crate::{
+    core::search::mcts::{
+        eval::Policy,
+        nn::RawLogits,
+        node::{NodeId, Tree, node_state::HasBranches},
+        search::{BatchItem, Selection},
+    },
+    misc::List,
+};
 use rand::{Rng, SeedableRng, rngs::SmallRng};
 
 use crate::core::{
@@ -75,10 +81,58 @@ impl Evaluator for DummyEvaluator {
             evaluations.push(Guess {
                 relative_to: trace_data.turn,
                 quality: Quality::new(quality),
-                policy: Policy::from_raw_logits(&raw_logits, moves, 1.0, &mut Box::new(List::new())),
+                policy: Policy::from_raw_logits(
+                    &raw_logits,
+                    moves,
+                    1.0,
+                    &mut Box::new(List::new()),
+                ),
             });
         }
 
         evaluations.into_iter()
     }
+}
+
+#[cfg(test)]
+#[test]
+#[ignore = "implementation in the search is missing and test is not complete yet"]
+pub fn chooses_shortest_mate() {
+    use crate::{
+        core::search::{
+            limit::UciLimit,
+            mcts::{HceParts, MctsConfig, SearchState, strategy::MctsUci},
+        },
+        misc::{CancellationToken, DebugMode},
+    };
+
+    struct Config;
+
+    impl MctsConfig for Config {
+        type Parts = HceParts;
+        type Strat = MctsUci;
+    }
+
+    // this position was reached during training with the following moves
+    // 47. Qf6+ Kh6 48. Qg5+ Kg7 49. Qf6+ Kxf6 50. Kg1 Bh2+ 51. Kh1 Bg1 *
+    // the nn failed to play the mate in 1, likely because it saw that the played
+    // sequence of moves was proven and did not search further for a quicker
+    // mate. consequently the selfplay loop cut the game of too early and even
+    // tho it would've been a mate in 1, it was declared a draw.
+    let mut pos = Position::from_fen("8/2P3kp/6p1/4p1Q1/P6P/6b1/1rq3B1/7K w - - 8 47").unwrap();
+    let mut state = SearchState::default();
+    let mut strat = MctsUci::new(
+        UciLimit {
+            iterations: 300,
+            ..Default::default()
+        },
+        DebugMode::off(),
+        CancellationToken::new(),
+        None,
+    );
+    let parts = HceParts::default();
+
+    super::mcts::<1, Config, _>(&mut pos, &parts, &mut state, &mut strat);
+
+    todo!()
 }
