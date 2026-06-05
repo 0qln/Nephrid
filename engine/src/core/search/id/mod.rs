65be97425111432d0e9cc41e50e319860860e873
0qln <0qln@proton.me>
2026-06-05 23:07:48 +0200

    no t2 killers

--- engine/src/core/search/id/mod.rs
+++ engine/src/core/search/id/mod.rs
@@ -249,7 +249,7 @@ impl Searcher {
             });
 
             let killers_t1 = &self.ss.entry(rel_ply).killers;
-            let killers_t2 = rel_ply.checked_sub(2).map(|d| &self.ss.entry(d).killers);
+            // let killers_t2 = rel_ply.checked_sub(2).map(|d| &self.ss.entry(d).killers);
 
             // generate the see score outside of the move generation and the sorting, such
             // that it isn't computed for each comparison and we don't distrurb cache
@@ -281,9 +281,9 @@ impl Searcher {
                         200_000 - (age as i32 * 10_000)
                     }
                     // T2 killers (..190_000)
-                    else if let Some(age) = killers_t2.and_then(|k| k._position(&m)) {
-                        190_000 - (age as i32 * 10_000)
-                    }
+                    // else if let Some(age) = killers_t2.and_then(|k| k._position(&m)) {
+                    //     190_000 - (age as i32 * 10_000)
+                    // }
                     else {
                         let (from, to, _) = m.into();
                         let piece = pieces.get_piece(from);
