cd6551c54da1a17e039d6ac58d2ee8347e9bca52
0qln <0qln@proton.me>
2026-07-14 01:26:18 +0200

    test
    Score of ccbench/in/builds/nephrid-1fc2c2d vs
    ccbench/in/builds/nephrid-bb45cdd: 12 - 12 - 70  [0.500] 94
    ...      ccbench/in/builds/nephrid-1fc2c2d playing White: 8 - 6 - 34
    [0.521] 48
    ...      ccbench/in/builds/nephrid-1fc2c2d playing Black: 4 - 6 - 36
    [0.478] 46
    ...      White vs Black: 14 - 10 - 70  [0.521] 94
    Elo difference: 0.0 +/- 35.6, LOS: 50.0 %, DrawRatio: 74.5 %
    SPRT: llr -0.00604 (-0.2%), lbound -2.94, ubound 2.94
    
    Player: ccbench/in/builds/nephrid-1fc2c2d
       "Draw by 3-fold repetition": 67
       "Draw by insufficient mating material": 3
       "Loss: Black mates": 6
       "Loss: White mates": 6
       "No result": 6
       "Win: Black mates": 4
       "Win: White mates": 8
    Player: ccbench/in/builds/nephrid-bb45cdd
       "Draw by 3-fold repetition": 67
       "Draw by insufficient mating material": 3
       "Loss: Black mates": 4
       "Loss: White mates": 8
       "No result": 6
       "Win: Black mates": 6
       "Win: White mates": 6
    Finished match

--- engine/src/core/params.rs
+++ engine/src/core/params.rs
@@ -415,7 +415,7 @@ impl const QSearchParams for C_IdNnueParams {
 }
 
 impl const IdParams for C_IdNnueParams {
-    fn nmp_reduction(&self) -> Depth { Depth::new(1) }
+    fn nmp_reduction(&self) -> Depth { Depth::new(3) }
     fn nmp_phase_threshold(&self) -> TaperValue { TaperValue::new(9) }
     fn nmp_depth_factor(&self) -> u8 { 3 }
     fn nmp_phase_factor(&self) -> u32 { 7 }
