6a75e9fb5ca009e8ec9142a23af17ccf4e909a48
0qln <0qln@proton.me>
2026-07-14 02:34:04 +0200

    tuning
    Score of ccbench/in/builds/nephrid-310abf5 vs
    ccbench/in/builds/nephrid-bb45cdd: 39 - 49 - 204  [0.483] 292
    ...      ccbench/in/builds/nephrid-310abf5 playing White: 21 - 27 - 98
    [0.479] 146
    ...      ccbench/in/builds/nephrid-310abf5 playing Black: 18 - 22 - 106
    [0.486] 146
    ...      White vs Black: 43 - 45 - 204  [0.497] 292
    Elo difference: -11.9 +/- 21.9, LOS: 14.3 %, DrawRatio: 69.9 %
    SPRT: llr -0.207 (-7.0%), lbound -2.94, ubound 2.94
    
    Player: ccbench/in/builds/nephrid-310abf5
       "Draw by 3-fold repetition": 200
       "Draw by fifty moves rule": 1
       "Draw by insufficient mating material": 2
       "Draw by stalemate": 1
       "Loss: Black mates": 27
       "Loss: White mates": 22
       "No result": 6
       "Win: Black mates": 18
       "Win: White mates": 21
    Player: ccbench/in/builds/nephrid-bb45cdd
       "Draw by 3-fold repetition": 200
       "Draw by fifty moves rule": 1
       "Draw by insufficient mating material": 2
       "Draw by stalemate": 1
       "Loss: Black mates": 18
       "Loss: White mates": 21
       "No result": 6
       "Win: Black mates": 27
       "Win: White mates": 22
    Finished match

--- engine/src/core/params.rs
+++ engine/src/core/params.rs
@@ -419,10 +419,10 @@ impl const ChronoParams for C_IdNnueParams {
 }
 
 impl const QSearchParams for C_IdNnueParams {
-    fn futility_margin(&self) -> AnyScore { AnyScore::new(202) }
+    fn futility_margin(&self) -> AnyScore { AnyScore::new(203) }
     fn delta_pruning_threshold(&self) -> TaperValue { TaperValue::new(2) }
     fn movecount_pruning_factor(&self) -> AnyScore { AnyScore::new(-22) }
-    fn phase_pruning_factor(&self) -> AnyScore { AnyScore::new(76) }
+    fn phase_pruning_factor(&self) -> AnyScore { AnyScore::new(80) }
 }
 
 impl const IdParams for C_IdNnueParams {
