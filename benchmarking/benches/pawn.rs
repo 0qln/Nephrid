f109e942ae4d8adb81d02c63428a9900f7194c2d
0qln <0qln@proton.me>
2026-07-17 04:00:28 +0200

    add pawn ep constructor benchmark

--- benchmarking/benches/pawn.rs
+++ benchmarking/benches/pawn.rs
@@ -48,5 +48,42 @@ pub fn move_iter_check_none(c: &mut Criterion) {
     }
 }
 
-criterion_group!(benches, pawn_attacks, move_iter_check_none);
+pub fn move_iter_ep_overhead(c: &mut Criterion) {
+    magics::init();
+    zobrist::init();
+
+    let mut group = c.benchmark_group("pawn::move_iter::ep");
+
+    // Active En Passant Square
+    let fen_with_ep = "k7/8/8/3pP3/8/8/8/3K4 w - d6 0 1";
+    let pos_with_ep = Position::from_fen(fen_with_ep).unwrap();
+
+    group.bench_with_input(BenchmarkId::new("active_ep_validation", fen_with_ep), &pos_with_ep, |b, pos| {
+        b.iter(|| {
+            dbg_fold_moves::<Pawn, NoCheck, AllLegal, _, _, _>(
+                black_box(pos),
+                black_box(0),
+                black_box(|acc, m: Move| ControlFlow::Continue::<(), _>(acc ^ m.get_to().v())),
+            )
+        })
+    });
+
+    // No En Passant Square
+    let fen_no_ep = "k7/8/8/3pP3/8/8/8/3K4 w - - 0 1";
+    let pos_no_ep = Position::from_fen(fen_no_ep).unwrap();
+
+    group.bench_with_input(BenchmarkId::new("early_exit_ep", fen_no_ep), &pos_no_ep, |b, pos| {
+        b.iter(|| {
+            dbg_fold_moves::<Pawn, NoCheck, AllLegal, _, _, _>(
+                black_box(pos),
+                black_box(0),
+                black_box(|acc, m: Move| ControlFlow::Continue::<(), _>(acc ^ m.get_to().v())),
+            )
+        })
+    });
+
+    group.finish();
+}
+
+criterion_group!(benches, pawn_attacks, move_iter_check_none, move_iter_ep_overhead);
 criterion_main!(benches);
