e2f46de40bba40d8be44d857bcd7974a708518a7
0qln <0qln@proton.me>
2026-07-27 00:27:47 +0200

    test
    Score of ccbench/in/builds/nephrid-f802c1a vs
    ccbench/in/builds/nephrid-739dfa5: 32 - 61 - 107  [0.427] 200
    ...      ccbench/in/builds/nephrid-f802c1a playing White: 18 - 30 - 52
    [0.440] 100
    ...      ccbench/in/builds/nephrid-f802c1a playing Black: 14 - 31 - 55
    [0.415] 100
    ...      White vs Black: 49 - 44 - 107  [0.512] 200
    Elo difference: -50.7 +/- 32.8, LOS: 0.1 %, DrawRatio: 53.5 %
    SPRT: llr -0.378 (-12.8%), lbound -2.94, ubound 2.94
    
    Player: ccbench/in/builds/nephrid-f802c1a
       "Draw by 3-fold repetition": 101
       "Draw by insufficient mating material": 6
       "Loss: Black mates": 30
       "Loss: White mates": 31
       "Win: Black mates": 14
       "Win: White mates": 18
    Player: ccbench/in/builds/nephrid-739dfa5
       "Draw by 3-fold repetition": 101
       "Draw by insufficient mating material": 6
       "Loss: Black mates": 14
       "Loss: White mates": 18
       "Win: Black mates": 30
       "Win: White mates": 31

--- engine/src/core/search/id/mod.rs
+++ engine/src/core/search/id/mod.rs
@@ -901,7 +901,7 @@ where
                 // penalize capture history heuristic that were expected but failed to not fail
                 // low
                 if is_capture && !is_promo {
-                    let ch_bonus = HistoryScore::new(5 + (depth.v() as THistoryScore * 2).pow(2));
+                    let ch_bonus = HistoryScore::new(20 + (depth.v() as THistoryScore * 5));
                     let capt_sq = m
                         .get_capture_sq()
                         .expect("we only get here if its a capture, which means it should also have a capt square. ");
@@ -923,7 +923,7 @@ where
         let (bm_from, bm_to, bm_flag) = best_move.into();
         if bm_flag.is_capture() && !bm_flag.is_promo() {
             // reward capture history heuristic
-            let ch_bonus = HistoryScore::new(7 + (depth.v() as THistoryScore * 3).pow(2));
+            let ch_bonus = HistoryScore::new(30 + (depth.v() as THistoryScore * 7));
             let capt_sq = best_move
                 .get_capture_sq()
                 .expect("we only get here if its a capture, which means it should also have a capt square. ");
